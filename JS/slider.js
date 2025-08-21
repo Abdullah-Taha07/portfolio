a33eabcaca563ef8132d529635693b80cb9e4828
AbdallaTaha07 <abduuallah3@gmail.com>
2025-08-21 15:08:05 +0300

    version 1.1

---
+++ JS/slider.js
@@ -0,0 +1,14 @@
+var $links = $(".itemLinks");
+
+$links.click(function (e) {
+    $links.removeClass("active");
+    var clickedLink = e.target;
+    clickedLink = $(clickedLink);
+    var position = clickedLink.attr("data-pos");
+    var translatevalue = "translateX("+ position * 25 +"%)";
+    $("#wrapper").css({
+        transform: translatevalue
+    });
+    clickedLink.addclass("active")
+});
+$($links[0]).addclass("active");
